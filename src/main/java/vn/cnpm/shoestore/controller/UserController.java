be6f7c5740cbbe95f1ec356d07abcc76aff80415
LeMinhDuc47 <fmgduc47@gmail.com>
2025-02-15 01:24:16 +0700

    set up jsp and jstl

---
+++ src/main/java/vn/cnpm/shoestore/controller/UserController.java
@@ -0,0 +1,37 @@
+package vn.cnpm.shoestore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import vn.cnpm.shoestore.domain.User;
+import vn.cnpm.shoestore.service.UserService;
+
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping("/")
+    public String getHomePage(Model model) {
+        return new String();
+    }
+
+    @RequestMapping("/admin/user")
+    public String getUserPage(Model model) {
+        model.addAttribute("newUser", new User());
+        return "admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
+    public String createUserPage(Model model, @ModelAttribute("newUser") User minhduc) {
+        System.out.println(minhduc);
+        return "";
+    }
+}
---
+++ src/main/java/vn/cnpm/shoestore/service/UserService.java
@@ -0,0 +1,8 @@
+package vn.cnpm.shoestore.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+}
