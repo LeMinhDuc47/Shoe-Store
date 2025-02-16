0e565ab6ad4f814126da92be075a0e43e891889d
LeMinhDuc47 <fmgduc47@gmail.com>
2025-02-16 20:12:05 +0700

    Temporary Template integration

--- src/main/java/vn/cnpm/shoestore/controller/admin/UserController.java
+++ src/main/java/vn/cnpm/shoestore/controller/admin/UserController.java
@@ -1,9 +1,5 @@
 package vn.cnpm.shoestore.controller.admin;
 
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.List;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -19,11 +15,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-
-import jakarta.servlet.ServletContext;
 
 @Controller
 public class UserController {
