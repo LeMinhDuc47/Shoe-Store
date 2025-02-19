1dfe11abc6d484292e1445123598906285ede6f0
LeMinhDuc47 <fmgduc47@gmail.com>
2025-02-19 09:16:31 +0700

    Cart function

--- src/main/java/vn/cnpm/shoestore/domain/User.java
+++ src/main/java/vn/cnpm/shoestore/domain/User.java
@@ -9,6 +9,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
@@ -39,6 +40,9 @@ public class User {
     private Role role;
     @OneToMany(mappedBy = "user")
     private List<Order> orders;
+
+    @OneToOne(mappedBy = "user")
+    private Cart cart;
 
     public String getAvatar() {
         return avatar;
@@ -112,10 +116,17 @@ public class User {
         this.phone = phone;
     }
 
+    public Cart getCart() {
+        return cart;
+    }
+
+    public void setCart(Cart cart) {
+        this.cart = cart;
+    }
+
     @Override
     public String toString() {
         return "User [id=" + id + ", email=" + email + ", password=" + password + ", fullName=" + fullName
                 + ", address=" + address + ", phone=" + phone + ", avatar=" + avatar + "]";
     }
-
 }
