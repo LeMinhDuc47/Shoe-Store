b93a4ac7e0ff05693dcf9cebfac9c950b3becce3
LeMinhDuc47 <fmgduc47@gmail.com>
2025-02-22 09:29:40 +0700

    Cart detail

--- src/main/java/vn/cnpm/shoestore/controller/client/ItemController.java
+++ src/main/java/vn/cnpm/shoestore/controller/client/ItemController.java
@@ -1,5 +1,7 @@
 package vn.cnpm.shoestore.controller.client;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +10,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
+import vn.cnpm.shoestore.domain.Cart;
+import vn.cnpm.shoestore.domain.CartDetail;
 import vn.cnpm.shoestore.domain.Product;
+import vn.cnpm.shoestore.domain.User;
 import vn.cnpm.shoestore.service.ProductService;
 
 @Controller
@@ -33,6 +38,28 @@ public class ItemController {
         String email = (String) session.getAttribute("email");
         this.productService.handleAddProductToCart(email, id, session);
         return "redirect:/";
+    }
+
+    @GetMapping("/cart")
+    public String getCartPage(Model model, HttpServletRequest request) {
+        User currentUser = new User();// null
+        HttpSession session = request.getSession(false);
+        long id = (long) session.getAttribute("id");
+        currentUser.setId(id);
+
+        Cart cart = this.productService.fetchByUser(currentUser);
+
+        List<CartDetail> cartDetails = cart.getCartDetails();
+
+        double totalPrice = 0;
+        for (CartDetail cd : cartDetails) {
+            totalPrice += cd.getPrice() * cd.getQuantity();
+        }
+
+        model.addAttribute("cartDetails", cartDetails);
+        model.addAttribute("totalPrice", totalPrice);
+
+        return "client/cart/show";
     }
 
 }
