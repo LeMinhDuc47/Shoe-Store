1e7bd5ac8095cf138b591900c54dd4908dcf418f
LeMinhDuc47 <fmgduc47@gmail.com>
2025-02-22 08:59:02 +0700

    Fix duplicating errors in the cart

--- src/main/java/vn/cnpm/shoestore/repository/CartDetailRepository.java
+++ src/main/java/vn/cnpm/shoestore/repository/CartDetailRepository.java
@@ -3,8 +3,11 @@ package vn.cnpm.shoestore.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import vn.cnpm.shoestore.domain.Cart;
 import vn.cnpm.shoestore.domain.CartDetail;
+import vn.cnpm.shoestore.domain.Product;
 
 @Repository
 public interface CartDetailRepository extends JpaRepository<CartDetail, Long> {
+    CartDetail findByCartAndProduct(Cart cart, Product product);
 }
